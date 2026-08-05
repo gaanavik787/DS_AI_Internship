c7f8a67a1c66bd3c4e84e2f79e3ab1fbfd9f4727
gaanavik787 <gaanavik787@gmail.com>
2026-08-05 11:42:13 +0530

    lists

---
+++ DAY2/list.py
@@ -0,0 +1,8 @@
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(type(mylist))
