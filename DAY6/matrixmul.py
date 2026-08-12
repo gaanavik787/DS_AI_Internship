51052031dd06806980554f0d262fa378501d275d
gaanavik787 <gaanavik787@gmail.com>
2026-08-12 14:54:06 +0530

    task1

--- DAY6/matrixmul.py
+++ DAY6/matrixmul.py
@@ -7,24 +7,29 @@ B = np.array([[7, 8],
               [9, 10],
               [11, 12]])
 
-C = A @ B 
+# Matrix multiplication
+C = A @ B
 print("Matrix product C:")
 print(C)
 
+# Transpose
 A_T = A.T
 print("Transpose of A:")
 print(A_T)
 
- M= np.array([[1, 2, 3],
+# Square matrix
+M = np.array([[1, 2, 3],
               [4, 5, 6],
               [7, 8, 10]])
+
 print("Square matrix M:")
 print(M)
 
-
+# Determinant
 det_M = np.linalg.det(M)
 print("Determinant of M:", det_M)
 
+# Inverse
 if det_M != 0:
     inv_M = np.linalg.inv(M)
     print("Inverse of M:")
